dfdc7d3671211460bbe5e0ceda9e8cf1138fb70b
Dmitry Nastich-Usov <design@webobraz.com>
2023-10-03 13:33:28 -0700

    NMP installing

--- node_experiments/test.js
+++ node_experiments/test.js
@@ -1,6 +1,8 @@
+// Node.js
 var fs = require('fs');
 var https = require('https');
 
+// Создаем файл с Н1 заголвком внутри
 fs.writeFile(__dirname + "/index.html", "<h1>HTML from node</h1>", function(error){
     if (error) {
         return console.log(error);
@@ -8,7 +10,7 @@ fs.writeFile(__dirname + "/index.html", "<h1>HTML from node</h1>", function(erro
       return console.log("Congrats");
     }
 })
-
+// Скачиваем каотинку из интернета в туже папку
 var myPhotoLocation = 'https://raw.githubusercontent.com/LearnWebCode/welcome-to-git/master/images/dog.jpg'
 https.get(myPhotoLocation, function(response){
     response.pipe(fs.createWriteStream(__dirname + "/mydog.jpg"));
