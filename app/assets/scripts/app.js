b145911cf96122d32cb1a32e61a94bca41f5cd0b
Dmitry Nastich-Usov <design@webobraz.com>
2023-10-19 11:44:21 -0700

    start with Java Script

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,5 +1,15 @@
 import '../styles/styles.css'
+import Person from './modules/person';
 
 if (module.hot) {
     module.hot.accept()
 }
+
+/* Lesson example code below this line*/
+
+
+let john = new Person("John Doe", "red");
+john.greet();
+
+let jane = new Person("Jane Smith", "green");
+jane.greet();
