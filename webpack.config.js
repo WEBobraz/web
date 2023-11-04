f4704bcaf8c2a42c834664f1bd5a9cc21d60c137
Dmitry Nastich-Usov <design@webobraz.com>
2023-11-03 21:34:13 -0700

    Complete website

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,14 @@ let config = {
   entry: "./app/assets/scripts/App.js",
   plugins: pages,
   module: {
-    rules: [cssConfig]
+    rules: [
+      cssConfig,
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: { loader: "babel-loader", options: { presets: ["@babel/preset-react", "@babel/preset-env"] } }
+      }
+    ]
   }
 }
 
@@ -62,11 +69,7 @@ if (currentTask == "dev") {
 }
 
 if (currentTask == "build") {
-  config.module.rules.push({
-    test: /\.js$/,
-    exclude: /(node_modules)/,
-    use: { loader: "babel-loader", options: { presets: ["@babel/preset-env"] } }
-  })
+
 
   cssConfig.use.unshift(MiniCssExtractPlugin.loader)
   config.output = {
